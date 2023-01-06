cb753d68538becd107850cb018d067bf58109d54
Ethan <ethannwaike@gmail.com>
2023-01-06 00:28:01 -0600

    Removing unused lines of code

--- src/server/Services/HitboxService/init.lua
+++ src/server/Services/HitboxService/init.lua
@@ -1,6 +1,5 @@
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local ServerScriptService = game:GetService("ServerScriptService")
 local Knit = require(ReplicatedStorage.Packages.Knit)
 
 local HitboxClass = require(script.HitboxClass)
@@ -30,29 +29,4 @@ function HitboxService:KnitInit()
 	print("HitboxService Initialized")
 end
 
---[[ multiple football warnings
-
-coroutine.wrap(function()
-	while task.wait(1) do
-		local NumOfFootballs = 0
-		for i, v in pairs(workspace:GetChildren()) do
-			if v.Name == 'Football' then
-				NumOfFootballs = NumOfFootballs+1
-			end
-		end
-		for i, v in pairs(game.Players:GetChildren()) do
-			if v.Character and v.Character:FindFirstChild('Football') and v.Character.Football:IsA('Tool') and v.Character.Football:FindFirstChild('Handle') then
-				NumOfFootballs = NumOfFootballs+1
-				game.ReplicatedStorage.GameValues.Ball.Value = v.Character.Football.Handle
-			end
-		end
-		if NumOfFootballs > 1 then
-			game.ReplicatedStorage:WaitForChild('MsgEvent'):FireAllClients('Warning',true)	
-		else
-			game.ReplicatedStorage:WaitForChild('MsgEvent'):FireAllClients('Warning',false)	
-		end
-	end
-end)()
-
-]]
 return HitboxService
