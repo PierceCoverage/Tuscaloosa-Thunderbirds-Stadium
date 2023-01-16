357e58c6bd85fb8054aa14ee9e13d461196ddef1
Ethan <ethannwaike@gmail.com>
2023-01-16 16:56:52 -0600

    HomeTeam & AwayTeam functionality & QOL update

--- src/server/Services/TeamService/init.lua
+++ src/server/Services/TeamService/init.lua
@@ -23,83 +23,6 @@ function TeamService.Client:GetAwayTeam()
 	return self.Server._AwayTeam
 end
 
-function TeamService:KnitStart()
-	local function create_team(Name: string, Color: BrickColor, AutoAssignable: boolean): Team
-		local Team = Instance.new("Team")
-		Team.Name = Name
-		Team.TeamColor = Color
-		Team.AutoAssignable = AutoAssignable
-		Team.Parent = Teams
-		return Team
-	end
-
-	for _, default_team in pairs(script.Default:GetChildren()) do
-		task.spawn(function()
-			local team = require(default_team)
-
-			local team_object = create_team(team.Name, team.Color, team.AutoAssignable)
-
-			if team.Callback then
-				team.Callback(team_object)
-			end
-
-			if team_object.Name == "Away" then
-				self._AwayTeam = team
-			end
-		end)
-	end
-
-	local GameService = Knit.GetService("GameService")
-	GameService.Values.Home.Team = Teams:FindFirstChild(self._HomeTeam.TeamData.Name)
-	GameService.Values.Away.Team =
-		Teams:FindFirstChild(self._AwayTeam.TeamData and self._AwayTeam.TeamData.Name or "Away")
-
-	task.spawn(function()
-		for i, v in pairs(workspace.Stadium.Seating:GetChildren()) do
-			if v:IsA("BasePart") then
-				if v.BrickColor == BrickColor.new("Bright red") then
-					v.Color = self._HomeTeam.ColorData.Primary.Color
-				elseif v.BrickColor == BrickColor.new("Navy blue") then
-					v.Color = self._HomeTeam.ColorData.Secondary.Color
-				elseif v.BrickColor == BrickColor.new("Smoky grey") then
-					v.Color = Color3.fromRGB(30, 30, 30)
-				end
-			end
-		end
-
-		for i, v in pairs(workspace.Stadium.SeatTarp:GetDescendants()) do
-			if v:IsA("BasePart") and v.BrickColor == BrickColor.new("Navy blue") then
-				v.Color = self._HomeTeam.ColorData.Secondary.Color
-			end
-		end
-
-		for i, v in pairs(workspace.Field["Field Grass"]:GetChildren()) do
-			if v.Name == "Endzone" then
-				v.Color = self._HomeTeam.ColorData.Primary.Color
-			end
-		end
-	end)
-	print("TeamService Started")
-end
-
-function TeamService:KnitInit()
-	for _, module in pairs(script.Teams:GetChildren()) do
-		local team = require(module)
-		table.insert(self._TeamData, team)
-		if team.TeamData.PlaceId == game.PlaceId then
-			self._HomeTeam = team
-
-			local Team = Instance.new("Team")
-			Team.Name = team.TeamData.Name
-			Team.TeamColor = team.ColorData.Primary.BrickColor
-			Team.AutoAssignable = false
-			Team.Parent = Teams
-		end
-	end
-
-	print("TeamService Initialized")
-end
-
 function TeamService:GetTeamData(team_name: string)
 	for i, v in pairs(self._TeamData) do
 		if v.TeamData.Name == team_name then
@@ -119,6 +42,166 @@ function TeamService:GetTeamFromRole(role_id: string)
 			return team.TeamData.Initials
 		end
 	end
+end
+
+local function create_team(Name: string, Color: BrickColor, AutoAssignable: boolean): Team
+	local Team = Instance.new("Team")
+	Team.Name = Name
+	Team.TeamColor = Color
+	Team.AutoAssignable = AutoAssignable
+	Team.Parent = Teams
+	return Team
+end
+
+function TeamService:SetAwayTeam(name: string)
+	local GameService = Knit.GetService("GameService")
+	local Away = GameService.Values.Away.Team
+	local Home = GameService.Values.Home.Team
+
+	local team = self:GetTeamData(name)
+
+	if not team then
+		return
+	end
+
+	if Home and name == Home.Name then
+		return
+	end
+
+	local color = team.ColorData.Primary.BrickColor
+
+	if Home and color == Home.TeamColor then
+		color = team.ColorData.Secondary.BrickColor
+	end
+
+	local TeamObject = create_team(team.TeamData.Name, team.ColorData.Primary.BrickColor, false)
+
+	for _, spawn in pairs(workspace.Spawns:GetChildren()) do
+		if spawn.Name == "Away" then
+			spawn.TeamColor = color
+		end
+	end
+
+	local team_players = {}
+
+	if Away then
+		team_players = Away:GetPlayers()
+		Away:Destroy()
+	end
+
+	self.AwayTeam = team
+	GameService:Update({ AwayTeam = TeamObject })
+
+	for _, v in pairs(team_players) do
+		v.Team = TeamObject
+	end
+end
+
+function TeamService:SetHomeTeam(name: string)
+	local GameService = Knit.GetService("GameService")
+	local Away = GameService.Values.Away.Team
+	local Home = GameService.Values.Home.Team
+
+	local team = self:GetTeamData(name)
+
+	if not team then
+		return
+	end
+
+	if Away and name == Away.Name then
+		return
+	end
+
+	local color = team.ColorData.Primary.BrickColor
+
+	if Away and color == Away.TeamColor then
+		Away.TeamColor = self._AwayTeam.ColorData.Secondary.BrickColor
+	end
+
+	local TeamObject = create_team(team.TeamData.Name, team.ColorData.Primary.BrickColor, false)
+
+	for _, spawn in pairs(workspace.Spawns:GetChildren()) do
+		if spawn.Name == "Team Spawn" then
+			spawn.TeamColor = color
+		end
+	end
+
+	for _, v in pairs(workspace.Stadium.Seating:GetChildren()) do
+		if v:IsA("BasePart") then
+			if v.Name == "PRIMARY" then
+				v.Color = team.ColorData.Primary.Color
+			elseif v.Name == "SECONDARY" then
+				v.Color = team.ColorData.Secondary.Color
+			elseif v.Name == "TERTIARY" then
+				v.Color = Color3.fromRGB(30, 30, 30)
+			end
+		end
+	end
+
+	for _, v in pairs(workspace.Stadium.SeatTarp:GetDescendants()) do
+		if v.Name == "SECONDARY" then
+			v.Color = team.ColorData.Secondary.Color
+		end
+	end
+
+	for _, v in pairs(workspace.Field["Field Grass"]:GetChildren()) do
+		if v.Name == "Endzone" then
+			v.Color = team.ColorData.Primary.Color
+		end
+	end
+
+	local team_players = {}
+
+	if Home then
+		team_players = Home:GetPlayers()
+		Home:Destroy()
+	end
+
+	self._HomeTeam = team
+	GameService:Update({ HomeTeam = TeamObject })
+
+	for _, v in pairs(team_players) do
+		v.Team = TeamObject
+	end
+end
+
+function TeamService:KnitStart()
+	local GameService = Knit.GetService("GameService")
+
+	for _, module in pairs(script.Teams:GetChildren()) do
+		local team = require(module)
+		table.insert(self._TeamData, team)
+		if team.TeamData.PlaceId == game.PlaceId then
+			self:SetHomeTeam(team.TeamData.Name)
+		end
+	end
+
+	for _, default_team in pairs(script.Default:GetChildren()) do
+		task.spawn(function()
+			local team = require(default_team)
+
+			local team_object = create_team(team.Name, team.Color, team.AutoAssignable)
+
+			if team.Callback then
+				team.Callback(team_object)
+			end
+
+			if team_object.Name == "Away" then
+				self._AwayTeam = team
+			end
+		end)
+	end
+
+	GameService:Update({
+		HomeTeam = Teams:FindFirstChild(self._HomeTeam.TeamData.Name),
+		AwayTeam = Teams:FindFirstChild(self._AwayTeam.TeamData and self._AwayTeam.TeamData.Name or "Away"),
+	})
+
+	print("TeamService Started")
+end
+
+function TeamService:KnitInit()
+	print("TeamService Initialized")
 end
 
 return TeamService
