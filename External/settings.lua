f636f50c6bdfa953ce0ab3d359b6688d43dedf02
Ethan <ethannwaike@gmail.com>
2023-01-18 00:35:01 -0600

    import settings to external

---
+++ External/settings.lua
@@ -0,0 +1,43 @@
+local settings = {
+
+	ToolLocation = game.ServerStorage.Tools,
+
+	DebugAdmin = true, -- Gives me creator admin to debug
+
+	MainScript = 11886224229, -- Don't change unless you know what you're doing
+
+	Prefix = ":",
+
+	GroupId = 0,
+
+	Creators = { "Ethan_Waike", "RNF", "Player1" }, -- Username:UserID'
+
+	Owners = {},
+
+	Admins = {},
+
+	Moderators = {},
+
+	Banned = {},
+
+	CommandPerms = {},
+
+	GroupAdmin = {
+		"16519943:255:Owners:false", --Com
+		"16519943:254:Owners:false", --CC
+		"16519943:253:Owners:false", --LT
+	}, -- GroupID:GroupRank:AdminLevel:AllAboveRanksIncluded
+
+	BanMessage = "You are banned.",
+
+	LockMessage = {}, --Slock WIP
+
+	LogLimit = 1000,
+}
+
+if game.PrivateServerOwnerId ~= 0 then
+	local username = game.Players:GetNameFromUserIdAsync(game.PrivateServerOwnerId)
+	table.insert(settings.Creators, username)
+end
+
+return settings
