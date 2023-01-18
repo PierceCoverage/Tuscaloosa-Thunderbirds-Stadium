487e9dbfd125396305af7b744016db9491d29c41
Ethan <ethannwaike@gmail.com>
2023-01-18 00:30:42 -0600

    Remove unneeded module

--- src/server/Import/Modern Admin Commands/Loader.server.lua
+++
@@ -1,4 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Knit = require(ReplicatedStorage.Packages.Knit)
-local settings = require(script.Parent.settings) --Don't change anything here
-require(settings.MainScript)
